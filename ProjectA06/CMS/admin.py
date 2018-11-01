08cc328cd2e419cbdd986e21ae19db246f7f6b18
= <=>
2018-11-01 10:46:55 +0530

    commit3

--- ProjectA06/CMS/admin.py
+++ ProjectA06/CMS/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(role)
+admin.site.register(student)
+admin.site.register(faculty)
+admin.site.register(college_admin)
+admin.site.register(course)
+admin.site.register(days)
+admin.site.register(timetable)
+admin.site.register(Grade)
+admin.site.register(Attendance)
+admin.site.register(Leave)
+admin.site.register(Query)
+admin.site.register(Answerqueryadmin)
+admin.site.register(Events)
